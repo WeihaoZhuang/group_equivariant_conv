7c9e2118f5fdc8c8f539e482b31a9e7a27b90585
Weihao <zhuangweihao1996@gmail.com>
2020-03-13 11:59:48 +0900

    cifar data

---
+++ cifar10/cifardata.py
@@ -0,0 +1,25 @@
+import torchvision.transforms as transforms
+import torchvision
+import torch
+
+def cifar10_dataloader(train_batchsize=128, test_batchsize=100):
+    # Data
+    print('==> Preparing data..')
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=train_batchsize, shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=test_batchsize, shuffle=False, num_workers=2)
+    return trainloader, testloader
