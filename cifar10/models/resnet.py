c922b9b5ad37ba5885e535dd5ee566cb7d1f0727
Weihao <zhuangweihao1996@gmail.com>
2020-03-13 19:43:34 +0900

    new push

--- cifar10/models/resnet.py
+++ cifar10/models/resnet.py
@@ -12,24 +12,42 @@ import torch.nn.functional as F
 
 from torch.autograd import Variable
 import sys
-sys.path.append("/home/weihao_zhuang/workspace/pytorch-gconv-experiments/")
+sys.path.append("/home/weihao_zhuang/workspace/group_equivariant_conv/")
 
 from groupy.gconv.pytorch_gconv import P4MConvZ2, P4MConvP4M
+
+def P4MConvZ2_3x3(in_planes, planes, kernel_size=3,stride=1,padding=1,bias=False):
+    return P4MConvZ2(in_planes, planes, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias)
+
+def P4MConvZ2_5x5(in_planes, planes, kernel_size=5,stride=1,padding=2,bias=False):
+    return P4MConvZ2(in_planes, planes, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias)
+
+def P4MConvP4M_3x3(in_planes, planes, kernel_size=3,stride=1,padding=1,bias=False):
+    return P4MConvP4M(in_planes, planes, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias)
+
+def P4MConvP4M_5x5(in_planes, planes, kernel_size=5,stride=1,padding=2,bias=False):
+    return P4MConvP4M(in_planes, planes, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias)
+
+def P4MConvP4M_1x1(in_planes, planes, kernel_size=1,stride=1,padding=0,bias=False):
+    return P4MConvP4M(in_planes, planes, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias)
+
+conv_p4m = P4MConvP4M_3x3
+conv_z2 = P4MConvZ2_3x3
 
 class BasicBlock(nn.Module):
     expansion = 1
 
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
-        self.conv1 = P4MConvP4M(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv1 = conv_p4m(in_planes, planes, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm3d(planes)
-        self.conv2 = P4MConvP4M(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv2 = conv_p4m(planes, planes, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm3d(planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
-                P4MConvP4M(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                P4MConvP4M_1x1(in_planes, self.expansion*planes, stride=stride, bias=False),
                 nn.BatchNorm3d(self.expansion*planes)
             )
 
@@ -46,17 +64,17 @@ class Bottleneck(nn.Module):
 
     def __init__(self, in_planes, planes, stride=1):
         super(Bottleneck, self).__init__()
-        self.conv1 = P4MConvP4M(in_planes, planes, kernel_size=1, bias=False)
+        self.conv1 = P4MConvP4M_1x1(in_planes, planes, bias=False)
         self.bn1 = nn.BatchNorm3d(planes)
-        self.conv2 = P4MConvP4M(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv2 = conv_p4m(planes, planes, stride=stride, bias=False)
         self.bn2 = nn.BatchNorm3d(planes)
-        self.conv3 = P4MConvP4M(planes, self.expansion*planes, kernel_size=1, bias=False)
+        self.conv3 = P4MConvP4M_1x1(planes, self.expansion*planes, bias=False)
         self.bn3 = nn.BatchNorm3d(self.expansion*planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
-                P4MConvP4M(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                P4MConvP4M_1x1(in_planes, self.expansion*planes, stride=stride, bias=False),
                 nn.BatchNorm3d(self.expansion*planes)
             )
 
@@ -70,11 +88,21 @@ class Bottleneck(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
+    def __init__(self, block, num_blocks, num_classes=10, conv_type = '3x3'):
         super(ResNet, self).__init__()
+        
+        global conv_z2
+        global conv_p4m
+        if conv_type == '3x3':
+            conv_p4m = P4MConvP4M_3x3
+            conv_z2 = P4MConvZ2_3x3
+        elif conv_type == '5x5':
+            conv_p4m = P4MConvP4M_5x5
+            conv_z2 = P4MConvZ2_5x5
+        
         self.in_planes = 23
 
-        self.conv1 = P4MConvZ2(3, 23, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv1 = conv_z2(3, 23, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm3d(23)
         self.layer1 = self._make_layer(block, 23, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 45, num_blocks[1], stride=2)
@@ -104,20 +132,20 @@ class ResNet(nn.Module):
         return out
 
 
-def ResNet18():
-    return ResNet(BasicBlock, [2,2,2,2])
+def ResNet18(conv_type='3x3'):
+    return ResNet(BasicBlock, [2,2,2,2],conv_type=conv_type)
 
-def ResNet34():
-    return ResNet(BasicBlock, [3,4,6,3])
+def ResNet34(conv_type='3x3'):
+    return ResNet(BasicBlock, [3,4,6,3],conv_type=conv_type)
 
-def ResNet50():
-    return ResNet(Bottleneck, [3,4,6,3])
+def ResNet50(conv_type='3x3'):
+    return ResNet(Bottleneck, [3,4,6,3],conv_type=conv_type)
 
-def ResNet101():
-    return ResNet(Bottleneck, [3,4,23,3])
+def ResNet101(conv_type='3x3'):
+    return ResNet(Bottleneck, [3,4,23,3],conv_type=conv_type)
 
-def ResNet152():
-    return ResNet(Bottleneck, [3,8,36,3])
+def ResNet152(conv_type='3x3'):
+    return ResNet(Bottleneck, [3,8,36,3],conv_type=conv_type)
 
 
 def test():
--- cifar10/utils_sho.py
+++ cifar10/utils_sho.py
@@ -1,28 +1,53 @@
 import groupy
 import torch
 import numpy as np
-mask = torch.tensor([[0,1,0],
-                     [1,1,1],
-                     [0,1,0]]).cuda()
 
-P4Conv_type = (groupy.gconv.pytorch_gconv.splitgconv2d.P4MConvZ2, 
-               groupy.gconv.pytorch_gconv.splitgconv2d.P4ConvP4, 
-               groupy.gconv.pytorch_gconv.splitgconv2d.P4MConvZ2,
-               groupy.gconv.pytorch_gconv.splitgconv2d.P4MConvP4M)
+# ker_hw = 5
 
-def grad_mul_mask(self, grad_input, grad_output):
-    """
-    """
+# Conv_type = (torch.nn.modules.conv.Conv2d,)
+# Conv_type = (groupy.gconv.pytorch_gconv.splitgconv2d.P4MConvZ2, 
+#                groupy.gconv.pytorch_gconv.splitgconv2d.P4ConvP4, 
+#                groupy.gconv.pytorch_gconv.splitgconv2d.P4MConvZ2,
+#                groupy.gconv.pytorch_gconv.splitgconv2d.P4MConvP4M)
+
+def create_mask(h):
+    mask = torch.zeros(size=(h,h), device='cuda')
+    pos = int(mask.shape[0]/2)
+    mask[pos] += 1.
+    mask[:,pos] += 1.
+    mask[pos,pos] = 1.
+    return mask
+
+# mask = create_mask(ker_hw)
+
+def grad_mul_mask_grad(self,*para):
     if self.weight.grad != None:
-        self.weight.grad *= mask                     
-               
-def mask_bwhook(net):
-    print("register backward hook")
-    for mod in select_layers(net):
-        if mod.kernel_size == (3,3):
-            mod.register_backward_hook(grad_mul_mask)         
-               
-def select_layers(model, ltype = P4Conv_type):
+        a = self.weight.grad * mask 
+        self.weight.grad = a 
+                
+def grad_mul_mask_data(self,*para):
+#     if self.weight.data != None:
+    mask = create_mask(self.weight.data.shape[-1])
+    a = self.weight.data * mask 
+    self.weight.data = a 
+
+def mask_hook(net, ltype, hook_type = 'forward_pre'):
+    for mod in select_layers(net, ltype):
+        if (mod.kernel_size == (3,3)) or (mod.kernel_size == (5,5)):
+            if hook_type == 'forward_pre':
+                print("register forward pre hook:", mod)
+                mod.register_forward_pre_hook(grad_mul_mask_data) 
+            elif hook_type == 'backward':
+                print("register backward hook:", mod)
+                mod.register_backward_hook(grad_mul_mask_grad)         
+                
+def init_skeleton_weight(net, ltype):
+    for mod in select_layers(net, ltype):
+        if (mod.kernel_size == (3,3)) or (mod.kernel_size == (5,5)):
+            mask = create_mask(mod.kernel_size[-1])
+            mod.weight.data *= mask
+    
+def select_layers(model, ltype):
     """
         Filter the submodules of model according to ltype.
     """
@@ -31,6 +56,7 @@ def select_layers(model, ltype = P4Conv_type):
 
 
 def check_para(tensor):
+    mask = create_mask(tensor.shape[-1])
     h = tensor.shape[-1]
     mask_check = torch.zeros(h,h).cuda()
     pos = int(mask_check.shape[0]/2)
@@ -40,7 +66,54 @@ def check_para(tensor):
     temp = (tensor != mask_check)
     mask_expand = mask.expand_as(tensor)
     np.testing.assert_allclose(temp.cpu().numpy(), mask_expand.cpu().numpy())
-    print("pass para checking")     
+    print("Pass para checking")     
+
+def check_net_paras(net, ltype):
+    for mod in select_layers(net, ltype):
+        if (mod.kernel_size == (3,3)) or (mod.kernel_size == (5,5)):
+            print("layer name:",mod," layer weight shape:",mod.weight.data.shape)
+            check_para(mod.weight.data)
+
+# mask = torch.tensor([[0,1,0],
+#                      [1,1,1],
+#                      [0,1,0]]).cuda()
+
+# P4Conv_type = (groupy.gconv.pytorch_gconv.splitgconv2d.P4MConvZ2, 
+#                groupy.gconv.pytorch_gconv.splitgconv2d.P4ConvP4, 
+#                groupy.gconv.pytorch_gconv.splitgconv2d.P4MConvZ2,
+#                groupy.gconv.pytorch_gconv.splitgconv2d.P4MConvP4M)
+
+# def grad_mul_mask(self, grad_input, grad_output):
+#     """
+#     """
+#     if self.weight.grad != None:
+#         self.weight.grad *= mask                     
+               
+# def mask_bwhook(net):
+#     print("register backward hook")
+#     for mod in select_layers(net):
+#         if mod.kernel_size == (3,3):
+#             mod.register_backward_hook(grad_mul_mask)         
+               
+# def select_layers(model, ltype = P4Conv_type):
+#     """
+#         Filter the submodules of model according to ltype.
+#     """
+#     check_ltype = lambda x: type(x) in ltype 
+#     return list(filter(check_ltype, model.modules()))    
+
+
+# def check_para(tensor):
+#     h = tensor.shape[-1]
+#     mask_check = torch.zeros(h,h).cuda()
+#     pos = int(mask_check.shape[0]/2)
+#     mask_check[pos] += 999.
+#     mask_check[:,pos] += 999.
+#     mask_check[pos,pos] = 999.
+#     temp = (tensor != mask_check)
+#     mask_expand = mask.expand_as(tensor)
+#     np.testing.assert_allclose(temp.cpu().numpy(), mask_expand.cpu().numpy())
+#     print("pass para checking")     
                
                
                
